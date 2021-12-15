333acfa9a3439e0db2d87d2152aceb3924edce0d
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2021-12-15 18:29:05 +0530

    Remove Koin from challenge project

--- 02-getting-started/projects/challenge/shared/src/iosMain/kotlin/com/raywenderlich/findtime/di/timezoneModule.kt
+++
@@ -1,9 +0,0 @@
-package com.raywenderlich.findtime.di
-
-import com.raywenderlich.findtime.TimeZoneHelper
-import com.raywenderlich.findtime.TimeZoneHelperImpl
-import org.koin.dsl.module
-
-actual fun timezoneModule() = module {
-    single<TimeZoneHelper> { TimeZoneHelperImpl() }
-}
