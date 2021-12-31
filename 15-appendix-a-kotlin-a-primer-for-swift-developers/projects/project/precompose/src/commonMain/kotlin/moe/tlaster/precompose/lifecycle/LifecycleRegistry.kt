3c5432073bd6a803677d90838b1a4b8ef7631b9c
Carlos Mota <cafonsomota@gmail.com>
2021-12-31 03:31:03 +0000

    Added appendix a project

---
+++ 15-appendix-a-kotlin-a-primer-for-swift-developers/projects/project/precompose/src/commonMain/kotlin/moe/tlaster/precompose/lifecycle/LifecycleRegistry.kt
@@ -0,0 +1,33 @@
+package moe.tlaster.precompose.lifecycle
+
+import moe.tlaster.precompose.standard.copyForEach
+
+class LifecycleRegistry : Lifecycle {
+    private val observers = arrayListOf<LifecycleObserver>()
+    override var currentState: Lifecycle.State = Lifecycle.State.Initialized
+        set(value) {
+            if (field == Lifecycle.State.Destroyed || value == Lifecycle.State.Initialized) {
+                return
+            }
+            field = value
+            dispatchState(value)
+        }
+
+    private fun dispatchState(value: Lifecycle.State) {
+        observers.copyForEach {
+            it.onStateChanged(value)
+        }
+    }
+
+    override fun removeObserver(observer: LifecycleObserver) {
+        observers.remove(observer)
+    }
+
+    override fun addObserver(observer: LifecycleObserver) {
+        observers.add(observer)
+    }
+
+    override fun hasObserver(): Boolean {
+        return observers.isNotEmpty()
+    }
+}
