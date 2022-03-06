ca1bf38fedf3451fcb13d94c69aaf5e3cefedb2e
Carlos Mota <478962+cmota@users.noreply.github.com>
2022-03-06 15:45:51 +0000

    Removed plugins

--- 11-serialization/projects/starter/plugins/multiplatform-swiftpackage-m1_support/src/main/kotlin/com/chromaticnoise/multiplatformswiftpackage/domain/DistributionURL.kt
+++
@@ -1,9 +0,0 @@
-package com.chromaticnoise.multiplatformswiftpackage.domain
-
-internal data class DistributionURL(val value: String) {
-    private val slashTerminatedValue: String get() =
-        value.takeIf { it.endsWith("/") } ?:
-        "$value/"
-
-    fun appendPath(path: String) = DistributionURL("$slashTerminatedValue$path")
-}
