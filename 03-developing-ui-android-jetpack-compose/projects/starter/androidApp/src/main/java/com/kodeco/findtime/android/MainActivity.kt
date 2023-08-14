3a99b22114238aac566f0199a139815ac796f61e
Kevin Moore <kevindavmoore@gmail.com>
2023-08-14 06:46:30 -0400

    Chapter 3 Updates

---
+++ 03-developing-ui-android-jetpack-compose/projects/starter/androidApp/src/main/java/com/kodeco/findtime/android/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.kodeco.findtime.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text("Hello, Android!")
+                }
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        Text("Hello, Android!")
+    }
+}
--- 03-developing-ui-android-jetpack-compose/projects/starter/iosApp/iosApp/ContentView.swift
+++ 03-developing-ui-android-jetpack-compose/projects/starter/iosApp/iosApp/ContentView.swift
@@ -2,8 +2,9 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
+
 	var body: some View {
-		Text("Hello")
+		Text("Hello iOS")
 	}
 }
 
