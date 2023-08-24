e61d43c4bdc83bca7de29c8c07b468f9e4186ca1
Godfred Afful <affulgodfred@gmail.com>
2023-08-24 21:56:30 +0000

    Revert "TE updates."
    This reverts commit 39d45abcafc753a91c07cbca2d616a02bc39e6d5.

--- 01-introduction/projects/final/build.gradle.kts
+++ 01-introduction/projects/final/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.1.0").apply(false)
-    id("com.android.library").version("8.1.0").apply(false)
+    id("com.android.application").version("8.2.0-alpha15").apply(false)
+    id("com.android.library").version("8.2.0-alpha15").apply(false)
     kotlin("android").version("1.8.21").apply(false)
     kotlin("multiplatform").version("1.8.21").apply(false)
 }
