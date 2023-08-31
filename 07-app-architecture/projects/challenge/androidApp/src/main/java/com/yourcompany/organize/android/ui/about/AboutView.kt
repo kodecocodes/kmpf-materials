9c188a7216571c97337c3e734c564f9d60ddfa7f
Saeed Taheri <saeed.taheri@gmail.com>
2023-08-31 20:09:17 +0400

    Update challenge project for chapter 7.

---
+++ 07-app-architecture/projects/challenge/androidApp/src/main/java/com/yourcompany/organize/android/ui/about/AboutView.kt
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2023 Kodeco LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.yourcompany.organize.android.ui.about
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.yourcompany.organize.Platform
+import com.yourcompany.organize.presentation.AboutViewModel
+import kotlin.math.max
+import kotlin.math.min
+
+@Composable
+fun AboutView(
+  viewModel: AboutViewModel = AboutViewModel(),
+  onUpButtonClick: () -> Unit
+) {
+  Column {
+    Toolbar(
+      title = viewModel.title,
+      onUpButtonClick = onUpButtonClick
+    )
+    ContentView(items = viewModel.items)
+  }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun Toolbar(
+  title: String,
+  onUpButtonClick: () -> Unit,
+) {
+  TopAppBar(
+    title = { Text(text = title) },
+    navigationIcon = {
+      IconButton(onClick = onUpButtonClick) {
+        Icon(
+          imageVector = Icons.Default.ArrowBack,
+          contentDescription = "Up Button",
+        )
+      }
+    }
+  )
+}
+
+@Composable
+private fun ContentView(items: List<AboutViewModel.RowItem>) {
+  LazyColumn(
+    modifier = Modifier.fillMaxSize(),
+  ) {
+    items(items) { row ->
+      RowView(title = row.title, subtitle = row.subtitle)
+    }
+  }
+}
+
+@Composable
+private fun RowView(
+  title: String,
+  subtitle: String,
+) {
+  Column(modifier = Modifier.fillMaxWidth()) {
+    Column(Modifier.padding(8.dp)) {
+      Text(
+        text = title,
+        style = MaterialTheme.typography.bodySmall,
+        color = Color.Gray,
+      )
+      Text(
+        text = subtitle,
+        style = MaterialTheme.typography.bodyLarge,
+      )
+    }
+    Divider()
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AboutPreview() {
+  AboutView {
+  }
+}
--- 07-app-architecture/projects/challenge/iosApp/iosApp/UI/About/AboutView.swift
+++ 07-app-architecture/projects/challenge/iosApp/iosApp/UI/About/AboutView.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2022 Razeware LLC
-/// 
+/// Copyright (c) 2023 Kodeco LLC
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -30,15 +30,17 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
+import Shared
 import SwiftUI
-import shared
 
 struct AboutView: View {
-  @Environment(\.dismiss) private var dismiss
-  @StateObject private var viewModel = AboutViewModel()
+  @Environment(\.dismiss)
+  private var dismiss
+
+  @State private var viewModel = AboutViewModel()
 
   var body: some View {
-    NavigationView {
+    NavigationStack {
       AboutListView(items: viewModel.items)
         .navigationTitle(viewModel.title)
         .toolbar {
@@ -59,7 +61,4 @@ struct AboutView_Previews: PreviewProvider {
   static var previews: some View {
     AboutView()
   }
-}
-
-extension AboutViewModel: ObservableObject {
 }
