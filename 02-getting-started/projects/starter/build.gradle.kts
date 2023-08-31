12da752b81fdca8b2532e3ae2e3053d85c7a754a
SubhrajyotiSen <subhrajyoti12@gmail.com>
2023-09-01 01:30:36 +0530

    Update AGP to work on AS Hedgehog Beta 1

--- 02-getting-started/projects/starter/build.gradle.kts
+++ 02-getting-started/projects/starter/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application").version("8.2.0-alpha16").apply(false)
-    id("com.android.library").version("8.2.0-alpha16").apply(false)
+    id("com.android.application").version("8.2.0-beta01").apply(false)
+    id("com.android.library").version("8.2.0-beta01").apply(false)
     kotlin("android").version("1.8.21").apply(false)
     kotlin("multiplatform").version("1.8.21").apply(false)
 }
