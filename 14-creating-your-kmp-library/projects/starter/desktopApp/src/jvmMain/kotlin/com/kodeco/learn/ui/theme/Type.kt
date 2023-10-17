9b90181dbacd71bd6d3aeb372b4fe5409e29d8c7
Carlos Mota <478962+cmota@users.noreply.github.com>
2023-10-17 17:45:26 +0100

    Chapter 14 - Starter Project Updates

--- 14-creating-your-kmp-library/projects/starter/desktopApp/src/jvmMain/kotlin/com/kodeco/learn/ui/theme/Type.kt
+++ 14-creating-your-kmp-library/projects/starter/desktopApp/src/jvmMain/kotlin/com/kodeco/learn/ui/theme/Type.kt
@@ -36,7 +36,9 @@ package com.kodeco.learn.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.Font
 import androidx.compose.ui.unit.sp
 
 private val fontSizeBig = 16.sp
@@ -44,49 +46,48 @@ private val fontSizeMedium = 15.sp
 private val fontSizeSmall = 14.sp
 private val fontSizeTiny = 12.sp
 
-// FIXME: There's an issue loading fonts with Compose Multiplatform 1.5.0
-/*private val OpenSansFontFamily = FontFamily(
+private val OpenSansFontFamily = FontFamily(
   Font("font/opensans_bold.ttf", FontWeight.Bold),
-  Font("font/opensans_extrabold", FontWeight.ExtraBold),
-  Font("font/opensans_light", FontWeight.Light),
-  Font("font/opensans_regular", FontWeight.Normal),
-  Font("font/opensans_semibold", FontWeight.SemiBold),
-)*/
+  Font("font/opensans_extrabold.ttf", FontWeight.ExtraBold),
+  Font("font/opensans_light.ttf", FontWeight.Light),
+  Font("font/opensans_regular.ttf", FontWeight.Normal),
+  Font("font/opensans_semibold.ttf", FontWeight.SemiBold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     headlineLarge = TextStyle(
-        //fontFamily = OpenSansFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = fontSizeMedium
     ),
 
     headlineMedium = TextStyle(
-        //fontFamily = OpenSansFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = fontSizeSmall
     ),
 
     headlineSmall = TextStyle(
-        //fontFamily = OpenSansFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = fontSizeTiny
     ),
 
     bodyLarge = TextStyle(
-        //fontFamily = OpenSansFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = fontSizeBig
     ),
 
     bodyMedium = TextStyle(
-        //fontFamily = OpenSansFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = fontSizeSmall
     ),
 
     bodySmall = TextStyle(
-        //fontFamily = OpenSansFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontSize = fontSizeSmall
     )
 )
