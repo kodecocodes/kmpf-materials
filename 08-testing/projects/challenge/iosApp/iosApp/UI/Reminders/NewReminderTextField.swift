eac8b71f53e5b106fb4ae9e50f3a0d8ef8f88817
Saeed Taheri <saeed.taheri@gmail.com>
2025-02-04 19:03:09 +0400

    Update challenge project for chapter 8.

--- 08-testing/projects/challenge/iosApp/iosApp/UI/Reminders/NewReminderTextField.swift
+++ 08-testing/projects/challenge/iosApp/iosApp/UI/Reminders/NewReminderTextField.swift
@@ -1,5 +1,5 @@
-/// Copyright (c) 2023 Kodeco LLC
-/// 
+/// Copyright (c) 2025 Kodeco LLC
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
@@ -46,9 +46,9 @@ struct NewReminderTextField: View {
   }
 }
 
-struct NewReminderTextField_Previews: PreviewProvider {
-  static var previews: some View {
-    NewReminderTextField(text: .constant("")) {
-    }
+#Preview {
+  @Previewable @State var textfieldText = ""
+
+  NewReminderTextField(text: $textfieldText) {
   }
 }
