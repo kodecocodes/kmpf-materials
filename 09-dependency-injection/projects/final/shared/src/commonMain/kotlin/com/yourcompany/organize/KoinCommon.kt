291ab09a92ae442fdfd614919cef6866bc3dc7d1
Saeed Taheri <saeed.taheri@gmail.com>
2025-02-06 20:09:28 +0400

    Update final project for chapter 9.

--- 09-dependency-injection/projects/final/shared/src/commonMain/kotlin/com/yourcompany/organize/KoinCommon.kt
+++ 09-dependency-injection/projects/final/shared/src/commonMain/kotlin/com/yourcompany/organize/KoinCommon.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Kodeco LLC
+ * Copyright (c) 2025 Kodeco LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -49,11 +49,11 @@ object Modules {
 
   val repositories = module {
     factory { RemindersRepository() }
-    factory { AboutViewModel(get()) }
   }
 
   val viewModels = module {
     factory { RemindersViewModel(get()) }
+    factory { AboutViewModel(get()) }
   }
 }
 
