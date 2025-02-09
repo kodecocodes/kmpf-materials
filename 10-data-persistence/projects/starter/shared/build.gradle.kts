3e3bb5425f0842754d76e11ad3a397e514e9b124
Saeed Taheri <saeed.taheri@gmail.com>
2025-02-09 21:33:06 +0400

    Update starter project of chapter 10.

--- 10-data-persistence/projects/starter/shared/build.gradle.kts
+++ 10-data-persistence/projects/starter/shared/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Kodeco LLC
+ * Copyright (c) 2025 Kodeco LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -32,14 +32,26 @@
  * THE SOFTWARE.
  */
 
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-  kotlin("multiplatform")
-  id("com.android.library")
-  id("org.jetbrains.compose")
+  alias(libs.plugins.kotlinMultiplatform)
+  alias(libs.plugins.androidLibrary)
+  alias(libs.plugins.composeMultiplatform)
+  alias(libs.plugins.composeCompiler)
 }
 
 kotlin {
-  androidTarget()
+  compilerOptions {
+    freeCompilerArgs.add("-Xexpect-actual-classes")
+  }
+
+  androidTarget {
+    compilerOptions {
+      jvmTarget.set(JvmTarget.JVM_17)
+    }
+  }
 
   jvm("desktop")
 
@@ -50,13 +62,17 @@ kotlin {
   ).forEach {
     it.binaries.framework {
       baseName = "Shared"
+      isStatic = true
     }
   }
+
+  applyDefaultHierarchyTemplate()
 
   sourceSets {
     val commonMain by getting {
       dependencies {
         implementation(compose.runtime)
+        implementation(project.dependencies.platform(libs.koin.bom))
         implementation(libs.koin.core)
         implementation(libs.kotlinx.datetime)
       }
@@ -69,57 +85,26 @@ kotlin {
     }
 
     val androidMain by getting {
-      dependsOn(commonMain)
       dependencies {
         implementation(libs.androidx.annotation)
         implementation(libs.androidx.lifecycle.viewmodel.ktx)
       }
     }
-
     val androidUnitTest by getting {
       dependencies {
+        implementation(kotlin("test-junit"))
         implementation(libs.junit)
-      }
-    }
-
-    val iosX64Main by getting
-    val iosArm64Main by getting
-    val iosSimulatorArm64Main by getting
-    val iosMain by creating {
-      dependsOn(commonMain)
-      iosX64Main.dependsOn(this)
-      iosArm64Main.dependsOn(this)
-      iosSimulatorArm64Main.dependsOn(this)
-    }
-
-    val iosX64Test by getting
-    val iosArm64Test by getting
-    val iosSimulatorArm64Test by getting
-    val iosTest by creating {
-      dependsOn(commonTest)
-      iosX64Test.dependsOn(this)
-      iosArm64Test.dependsOn(this)
-      iosSimulatorArm64Test.dependsOn(this)
-    }
-
-    val desktopMain by getting {
-      dependsOn(commonMain)
-      dependencies {
-        implementation(compose.desktop.common)
       }
     }
   }
 }
 
 android {
-  compileSdk = 34
+  compileSdk = libs.versions.android.compileSdk.get().toInt()
   namespace = "com.yourcompany.organize"
 
-  sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-  sourceSets["main"].res.srcDirs("src/androidMain/res")
-
   defaultConfig {
-    minSdk = 27
+    minSdk = libs.versions.android.minSdk.get().toInt()
   }
 
   compileOptions {
