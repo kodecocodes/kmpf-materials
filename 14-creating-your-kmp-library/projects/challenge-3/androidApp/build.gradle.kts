d0f5fd41f4fe2275f2c132e76fa8645c035707e6
Carlos Mota <cafonsomota@gmail.com>
2025-02-09 03:30:58 +0000

    Updated challenge 3 project

--- 14-creating-your-kmp-library/projects/challenge-3/androidApp/build.gradle.kts
+++ 14-creating-your-kmp-library/projects/challenge-3/androidApp/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Kodeco Inc
+ * Copyright (c) 2025 Kodeco Inc
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -35,12 +35,13 @@
 plugins {
   id("com.android.application")
   kotlin("android")
+
+  alias(libs.plugins.jetbrains.compose.compiler)
 }
 
 dependencies {
   implementation(project(":shared"))
-  implementation(project(":shared-logger"))
-  implementation(project(":shared-action"))
+  implementation("com.kodeco.shared:shared-action:1.0")
 
   implementation(libs.android.material)
   implementation(libs.androidx.navigation.compose)
@@ -65,8 +66,8 @@ android {
     applicationId = "com.kodeco.learn"
     minSdk = libs.versions.android.sdk.min.get().toInt()
     targetSdk = libs.versions.android.sdk.target.get().toInt()
-    versionCode = 2
-    versionName = "2.0"
+    versionCode = 3
+    versionName = "3.0"
   }
 
   buildTypes {
@@ -86,10 +87,6 @@ android {
 
   kotlinOptions {
     jvmTarget = "17"
-  }
-
-  composeOptions {
-    kotlinCompilerExtensionVersion = libs.versions.android.compose.compiler.get()
   }
 
   packaging {
